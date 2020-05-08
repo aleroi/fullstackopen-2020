0f7f262304530695b6e0e4a3234c836792d373d8
Aleksi Roitto <aleksiroitto@hotmail.com>
2020-05-08 17:51:50 +0300

    test test

---
+++ Osa3/puhelinluettelo/build/precache-manifest.31105efa54c7188213240801d71e17c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab05da2d432fe23ca29cd6461036e754",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bd3f6be9de477047073",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "6b3a6e21a272269c36fe",
+    "url": "/static/js/2.5fafef88.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5fafef88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2bd3f6be9de477047073",
+    "url": "/static/js/main.3f02a8fd.chunk.js"
+  },
+  {
+    "revision": "30a55457603743091da3",
+    "url": "/static/js/runtime-main.c6dbe2fa.js"
+  }
+]);
